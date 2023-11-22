99f0e4ad09ce1634d4c0ccbdf5942db1c27ba68f
WillyEsco <90744366+WillyEsco@users.noreply.github.com>
2023-11-22 15:51:07 -0300

    primeros pasos creacion incidente

--- README.md
+++ README.md
@@ -70,12 +70,12 @@ mostrando la solución generada y justificando las decisiones tomadas
 
 # **Diagrama de Clases** 
 <p align="center">
-  <img  src="./incidentes/assets/Clases.jpg" width="80%" height="80%" />
+  <img  src="./repoinci/assets/Clases.jpg" width="80%" height="80%" />
 </p>
 
 <br><br>
 
 # __Diagrama de Entidad Relación__ 
 <p align="center">
-  <img  src="./incidentes/assets/DER.jpg" width="80%" height="80%"  />
+  <img  src="./repoinci/assets/DER.jpg" width="80%" height="80%"  />
 </p>
--- repoinci/src/main/java/com/grupo01/App.java
+++ repoinci/src/main/java/com/grupo01/App.java
@@ -1,9 +1,22 @@
 package com.grupo01;
+import  com.grupo01.repoinci.src.entidades.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Alta de Incidente:" );
+        Incidente incidente = new Incidente();
+        incidente.setIncidenteID(1);
+        incidente.setFechaCreacion(new Date());
+        incidente.setTecnico(new Tecnico());
+        incidente.setFechaAsignacion(new Date());
+        incidente.setConsideracion("Consideracion");
+        incidente.setEstado(new Estado());
+        incidente.setDescripcionCliente("Descripcion Cliente");
+        incidente.setTiempoExtra(1);
+        incidente.setCliente(new Cliente());
+        System.out.println(incidente);
+        
     }
 }
